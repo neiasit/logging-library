43c22171ae5749c5e85e557d44355b1fa49c71f3
georgiygusev <Ge0rgiusGus@yandex.ru>
2024-07-18 01:50:07 +0300

    initial

--- pkg/fx_mod.go
+++ pkg/fx_mod.go
@@ -3,8 +3,6 @@ package pkg
 import (
 	"github.com/neiasit/logging-library/internal"
 	"go.uber.org/fx"
-	"go.uber.org/fx/fxevent"
-	"log/slog"
 )
 
 var Module = fx.Module(
@@ -13,9 +11,4 @@ var Module = fx.Module(
 		internal.LoadConfig,
 		internal.Logger,
 	),
-	fx.WithLogger(func(logger *slog.Logger) fxevent.Logger {
-		return &fxevent.SlogLogger{
-			Logger: logger,
-		}
-	}),
 )
